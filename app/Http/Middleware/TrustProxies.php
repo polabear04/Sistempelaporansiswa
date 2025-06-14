b3e6cb62a64b1dc8b8af6cdc31edd1cc84acd2f1
hidayatrpl <hidayatrpl1@gmail.com>
2025-06-14 17:05:02 +0700

    try10

--- app/Http/Middleware/TrustProxies.php
+++ app/Http/Middleware/TrustProxies.php
@@ -7,8 +7,18 @@ use Illuminate\Http\Middleware\TrustProxies as Middleware;
 
 class TrustProxies extends Middleware
 {
-    protected $proxies = '*';
+    /**
+     * The trusted proxies for this application.
+     *
+     * @var array|string|null
+     */
+    protected $proxies = '*'; // Percayai semua proxy (termasuk Railway)
 
+    /**
+     * The headers that should be used to detect proxies.
+     *
+     * @var int
+     */
     protected $headers =
     Request::HEADER_X_FORWARDED_FOR |
         Request::HEADER_X_FORWARDED_HOST |
